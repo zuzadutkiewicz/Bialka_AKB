e53c8e73f4c283aa6760a6eaff60be5b7f676efe
zuzadutkiewicz <zuza.dutkiewicz@gmail.com>
2016-01-12 22:18:26 +0100

    Dodanie tablicy skojarzen

--- main.cpp
+++ main.cpp
@@ -14,18 +14,30 @@ using namespace std;
 
 struct sekwencja
 {
-    char amino[MAX_LICZ_ELEM];
+    string amino;
     int wagi[MAX_LICZ_ELEM];
     int liczElem;
 };
 
 struct sekwencja instacja[ROZM_TAB];
+int **grafSkojarzen;
 
 void odczytajZPliku(const char * nazwaPliku);
+void porownaj();
+int porownajAmin(int wiersz1, int  kolumna1, int wiersz2, int kolumna2);
+int indexTablicy(int wiersz, int kolumna);
+void utworzGrafSkojarzen();
+void drukujGrafSkojarzen();
+
+int okno = 5;
+int wiarygodnosc = 10;
 
 int main()
 {
-odczytajZPliku("instacja.txt");
+    odczytajZPliku("instacja.txt");
+    utworzGrafSkojarzen();
+    porownaj();
+    drukujGrafSkojarzen();
 }
 
 
@@ -45,19 +57,11 @@ void odczytajZPliku(const char * nazwaPliku)
     while(getline(plik,lineAmin))
     {
 
-
-        const char * buf = lineAmin.c_str();
-
         int liczAmin = 0;
 
-        while(buf[liczAmin] !='\0')
-        {
-           instacja[element].amino[liczAmin] = buf[liczAmin];
-           cout << instacja[element].amino[liczAmin];
-           liczAmin++;
-
-        }
-        cout << endl;
+        instacja[element].amino = lineAmin;
+        liczAmin =  lineAmin.length() + 1;
+        cout << "Amin: " << instacja[element].amino[liczAmin] << endl;
         getline(plik,lineWag);
 
         const char * bufWag = lineWag.c_str();
@@ -84,11 +88,21 @@ void odczytajZPliku(const char * nazwaPliku)
                 j = 0;
             }
             if(bufWag[i] == '\0')
-            break;
+                break;
             i++;
         }
 
+        liczAmin-=1;
+        liczWag-=1;
+        if(liczAmin != liczWag)
+        {
+            cout << "Rozna liczba elementow na pozycji: " << element << endl;
+            cout << "Liczba Amin: " << liczAmin << endl;
+            cout << "Liczba Wag: " << liczWag << endl;
+            cout << "Wybieram mniejszą wartosc." << endl;
+        }
 
+        instacja[element].liczElem = liczAmin > liczWag ? liczWag : liczAmin;
 
         cout << endl << "Koniec instancji" << endl;
 
@@ -102,3 +116,57 @@ void odczytajZPliku(const char * nazwaPliku)
 }
 
 
+void porownaj()
+{
+    for (int wiersz1 = 0; wiersz1 < ROZM_TAB; wiersz1++)
+        for (int wiersz2 =0; wiersz2 < ROZM_TAB; wiersz2++)
+        {
+            if ( wiersz1 == wiersz2)
+                continue;
+            for (int kolumna1 = 0; kolumna1 < instacja[wiersz1].liczElem - okno; kolumna1++)
+                for (int kolumna2 =0; kolumna2 < instacja[wiersz2].liczElem - okno; kolumna2++)
+                {
+                    int ret = porownajAmin(wiersz1, kolumna1, wiersz2, kolumna2);
+
+                    if( ret == 1)
+                    {
+                        grafSkojarzen[indexTablicy(wiersz1, kolumna1)][indexTablicy(wiersz2, kolumna2)] =1;
+                        grafSkojarzen[indexTablicy(wiersz2, kolumna2)][indexTablicy(wiersz1, kolumna1)] =1;
+                        //    cout << " amino1 " << wiersz1 << " " << kolumna1 << " idx " << indexTablicy(wiersz1, kolumna1) << endl;
+                        //    cout << " amino2 " << wiersz2 << " " << kolumna2 << " idx " << indexTablicy(wiersz2, kolumna2) << endl << endl;
+                    }
+                }
+        }
+}
+
+
+int porownajAmin(int wiersz1, int kolumna1, int wiersz2, int kolumna2)
+{
+    string amino1 = instacja[wiersz1].amino.substr(kolumna1, okno);
+    string amino2 = instacja[wiersz2].amino.substr(kolumna2, okno);
+    if( amino1.compare(amino2) == 0)
+    {
+        return 1;
+    }
+    return 0;
+}
+
+int indexTablicy(int wiersz, int kolumna)
+{
+    return wiersz * MAX_LICZ_ELEM + kolumna;
+}
+
+void utworzGrafSkojarzen()
+{
+    grafSkojarzen = new int *[ROZM_TAB * MAX_LICZ_ELEM];
+    for ( int i = 0; i < ROZM_TAB * MAX_LICZ_ELEM; ++i )
+        grafSkojarzen[i] = new int [ROZM_TAB * MAX_LICZ_ELEM];
+}
+
+void drukujGrafSkojarzen()
+{
+    for(int i = 0; i < ROZM_TAB * MAX_LICZ_ELEM; i++)
+        for(int j = 0; j < ROZM_TAB * MAX_LICZ_ELEM; j++)
+            if(grafSkojarzen[i][j] == 1)
+                cout << "grafSkojarzen[" << i << "][" << j << "]=" << grafSkojarzen[i][j] << endl;
+}
