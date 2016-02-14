a4f4804033a954bf9c72d0066d58489da02db467
zuzadutkiewicz <zuzadutkiewicz@gmail.com>
2016-02-14 14:02:16 +0100

    Wersja do testow

--- main.cpp
+++ main.cpp
@@ -41,18 +41,15 @@ void drukujBialka();
 int okno = 5;
 int wiarygodnosc = 28;
 
-int maxOpuszczone = 20;
-int minLiczElem = 3;
-int glebokoscSzukania = 3;
+int maxOpuszczone = 30;
+int minLiczElem = 6;
+int glebokoscSzukania = 5;
+
 
 int main()
 {
     odczytajZPliku("instacja.txt");
-    int jest = generujBialka();
-    if( jest == 1)
-        drukujBialka();
-    else
-        cout << "Brak rozwiazan" << endl;
+    generujBialka();
 }
 
 
@@ -62,35 +59,48 @@ int generujBialka()
     int liczbaElem = 0;
     int skojarzKol = 0;
     int wiersz = 0;
+    int znalezione = 0;
     zerujSkojarzTablPoz();
-    for(int kolumna = 0; kolumna < MAX_LICZ_ELEM; kolumna++)
+    for(int kolumna = 0; kolumna < MAX_LICZ_ELEM - okno; kolumna++)
     {
         int skojarzWiersz = 0;
-        int znaleziony = nastepnyPoziom(skojarzWiersz, skojarzKol, wiersz, kolumna);
-        if(znaleziony == 1 )
+        int jest = nastepnyPoziom(skojarzWiersz, skojarzKol, wiersz, kolumna);
+        if(jest == 1 )
         {
             liczbaElem = liczbaElem + 1;
             skojarzTabl[skojarzWiersz][skojarzKol].wiersz = wiersz;
             skojarzTabl[skojarzWiersz][skojarzKol].kolumna = kolumna;
-            if ( liczbaElem > minLiczElem )
-                break;
+            skojarzKol++;
         }
         else
-            opuszczone = opuszczone + 1;
+             opuszczone = opuszczone + 1;
 
         if(opuszczone > maxOpuszczone)
         {
-            // wyczysc tablice i szukaj dalsze elementy
+            // drukuj i wyczysc tablice i szukaj dalsze elementow
+            if(liczbaElem >= minLiczElem)
+            {
+                cout << "Znalezione elementy:" << endl;
+                drukujBialka();
+                znalezione = 1;
+            }
             zerujSkojarzTablPoz();
+            liczbaElem = 0;
             skojarzKol = 0;
         }
         // przejscie do nastepnej kolumny
-        skojarzKol++;
+        // skojarzKol++;
     }
     if(liczbaElem >= minLiczElem)
-        return 1;
+    {
+        drukujBialka();
+    }
+    if( znalezione == 0)
+        cout << " Brak rozwiazan." << endl;
+
     return 0;
 }
+
 
 int nastepnyPoziom(int skojarzWiersz, int skojarzKol, int wiersz, int kolumna)
 {
@@ -98,20 +108,24 @@ int nastepnyPoziom(int skojarzWiersz, int skojarzKol, int wiersz, int kolumna)
     wiersz++;
     wiersz = wiersz % ROZM_TAB;
     skojarzWiersz++;
-    if(wiersz > glebokoscSzukania)
+    if(skojarzWiersz >= glebokoscSzukania)
         return 1;
     if(skojarzNast(skojarzWiersz, skojarzKol, wiersz, kolumna, &skojarzTmp) == 1)
-        if( nastepnyPoziom(skojarzWiersz, skojarzKol, wiersz, kolumna) == 1)
+    {
+        if( nastepnyPoziom(skojarzWiersz, skojarzKol, skojarzTmp.wiersz, skojarzTmp.kolumna) == 1)
         {
-            skojarzTabl[skojarzWiersz][skojarzKol].wiersz = skojarzTmp.wiersz;
+            skojarzTabl[skojarzWiersz][skojarzKol].wiersz  = skojarzTmp.wiersz;
             skojarzTabl[skojarzWiersz][skojarzKol].kolumna = skojarzTmp.kolumna;
             return 1;
         }
+    }
     return 0;
 }
 
+
 void drukujBialka()
 {
+    int drukPrzerwy = 0;
     for(int j = 0; j < MAX_LICZ_ELEM; j++)
     {
         for(int i = 0; i < ROZM_TAB; i++)
@@ -135,25 +149,33 @@ void drukujBialka()
                      << " amino:"   << amino
                      << " aminoW: " << aminoW
                      << endl;
+                drukPrzerwy = 1;
             }
         }
-        // cout << endl;
+        if( drukPrzerwy == 1)
+        {
+            cout << endl;
+            drukPrzerwy = 0;
+        }
     }
 }
 
 
 int skojarzNast(int skojarzWiersz, int skojarzKol, int wiersz, int kolumna, struct skojarz * skojarzTmp)
 {
-    for(int i = kolumna; i <= kolumna + maxOpuszczone; i++)
+    // na podstawie linia i kolumna ustalic poprzednie wartosci
+    // w danym wierszu z ewentualnym przesuniecie o maxOpuszczone
+    int kolumnaTmp = 0;
+
+    if(skojarzKol > 0)
+        kolumnaTmp = skojarzTabl[skojarzWiersz][skojarzKol - 1].kolumna + 1;
+
+    // nie moze przekraczac maksymalnej liczby elementow
+    int kolumnaMax = kolumnaTmp + maxOpuszczone > MAX_LICZ_ELEM ? MAX_LICZ_ELEM : kolumnaTmp + maxOpuszczone;
+
+    for(int i = kolumnaTmp; i <= kolumnaMax; i++)
     {
-        // na podstawie linia i kolumna ustalic poprzednie wartosci
-        // w tablicy skojarzen i sprawdzic czy sa odpowiednie skojarzenia
-        // w danym wierszu z ewentualnym przesuniecie o maxOpuszczone
-        int wiersz1  = wiersz - 1;
-        int kolumna1 = kolumna;
-        int wiersz2 = wiersz;
-        int kolumna2 = i;
-        int jestRowne = porownajAmin(wiersz1, kolumna1, wiersz2, kolumna2);
+        int jestRowne = porownajAmin(wiersz -1, kolumna, wiersz, i);
         if( jestRowne == 1 )
         {
             skojarzTmp->wiersz = wiersz;
@@ -178,11 +200,15 @@ void zerujSkojarzTablPoz()
 
 int porownajAmin(int wiersz1, int kolumna1, int wiersz2, int kolumna2)
 {
+    if(kolumna1 + okno > instancja[wiersz1].amino.length() )
+        return 0;
+    if(kolumna2 + okno > instancja[wiersz2].amino.length() )
+        return 0;
+
     string amino1 = instancja[wiersz1].amino.substr(kolumna1, okno);
     string amino2 = instancja[wiersz2].amino.substr(kolumna2, okno);
     string aminoW1 = "";
     string aminoW2 = "";
-
     if( amino1.compare(amino2) == 0)
         return 1;
 
@@ -195,7 +221,7 @@ int porownajAmin(int wiersz1, int kolumna1, int wiersz2, int kolumna2)
     }
 
     if ( aminoW1.compare("") == 0 || aminoW2.compare("") == 0 )
-        return 1;
+        return 0;
 
     size_t found1 = amino1.find(aminoW2);
     if ( found1 != string::npos )
@@ -204,7 +230,6 @@ int porownajAmin(int wiersz1, int kolumna1, int wiersz2, int kolumna2)
     size_t found2 = amino1.find(aminoW2);
     if ( found2 != string::npos )
         return 1;
-
 
     return 0;
 }
@@ -238,7 +263,7 @@ void odczytajZPliku(const char * nazwaPliku)
 
         instancja[element].amino = lineAmin;
         liczAmin =  lineAmin.length() + 1;
-        cout << "Amin: " << instancja[element].amino[liczAmin] << endl;
+        // cout << "Amin: " << instancja[element].amino[liczAmin] << endl;
         getline(plik,lineWag);
 
         const char * bufWag = lineWag.c_str();
@@ -260,7 +285,7 @@ void odczytajZPliku(const char * nazwaPliku)
                 buflicz[j] = '\0';
                 dana = atoi(buflicz);
                 instancja[element].wagi[liczWag] = dana;
-                cout << instancja[element].wagi[liczWag] << " ";
+                // cout << instancja[element].wagi[liczWag] << " ";
                 liczWag++;
                 j = 0;
             }
@@ -271,24 +296,16 @@ void odczytajZPliku(const char * nazwaPliku)
 
         liczAmin-=1;
         liczWag-=1;
-        if(liczAmin != liczWag)
-        {
-            cout << "Rozna liczba elementow na pozycji: " << element << endl;
-            cout << "Liczba Amin: " << liczAmin << endl;
-            cout << "Liczba Wag: " << liczWag << endl;
-            cout << "Wybieram mniejszą wartosc." << endl;
-        }
 
         instancja[element].liczElem = liczAmin > liczWag ? liczWag : liczAmin;
 
-        cout << endl << "Koniec instancji" << endl;
 
         cout << "Amin: " << element << "  " << lineAmin << endl;
-        cout << "Wag: " << element << "  " << lineWag << endl;
+        cout << "Wag: " << element << "  " << lineWag << endl << endl;
         element++;
     }
 
     plik.close();
-    printf("Odczyt z pliku %s zakonczony powodzeniem.\n", nazwaPliku);
+    printf("Odczyt z pliku %s zakonczony powodzeniem.\n\n", nazwaPliku);
 }
 
